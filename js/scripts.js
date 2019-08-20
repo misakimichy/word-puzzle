d216c466a1bb5d51ec86864ac14c2df003094b49
mk and zs <mk@>
2019-08-20 11:50:48 -0700

    Add for loop and convert vowels into dash.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var string = "Believe you can and you're halfway there. Theodore Roosevelt";
+    var sentences = string.split('');
+    console.log("split sentenes:", sentences);
+    var vowels = ['a', 'i', 'e', 'o', 'u'];
+    var array = [];
+    for (var i = 0; i < sentences.length; i++) {
+      for (var j = 0; j < vowels.length; j++) {
+        if(sentences[i] === vowels[j]) {
+          sentences[i] = '-';
+          array.push(sentences[i]);
+        }
+      }
+      array.push(sentences[i]);
+    }
+
+    console.log("array:", array);
+    $("#result").show();
+  });
+});
